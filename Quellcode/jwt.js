d3c584bcd3fcdff914d55b5ab60003355c06b45c
Joshua Gramatzki <joshua.gramatzki@tobit.com>
2023-06-09 14:17:33 +0200

    Initial commit

---
+++ Quellcode/jwt.js
@@ -0,0 +1,19 @@
+const header = {
+	"alg": "HS256",
+	"typ": "JWT"
+}
+
+const payload = {
+  "sub": "1234567890",
+  "name": "John Doe",
+  "iat": 1516239022
+}
+
+const signature = HMAC_SHA(
+	secret,
+	base64urlEncoding(header) + '.' + 
+	base64urlEncoding(payload)
+)
+
+
+const token = base64urlEncodingheader + '.' + base64urlEncoding(payload + '.' base64urlEncoding(signature)
