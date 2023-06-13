95cd3ac5d4c63d89c9f0944dc16db2926f6f75d5
Joshua Gramatzki <joshua.gramatzki@tobit.com>
2023-06-13 20:38:35 +0200

    :wrench: Update choutwords script to count till literature entries

--- countwords.sh
+++ countwords.sh
@@ -7,7 +7,7 @@ detex thesis_main.tex > thesis_main.txt
 
 # Removes everything before Einleitung and after Anhang
 BEGINNING="Einleitung"
-ENDING="Anhang"
+ENDING="Literaturverzeichnis"
 while read p; do
   # end txt when end was found
   echo "$p" | grep $ENDING && break
